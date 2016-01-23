8f2bdfc50820f6385171d5325f05f2b7bbc5255b
Aaron Jenner <aaronjenner13@gmail.com>
2016-01-23 21:42:27 +0000

    Simple Breakout Game

---
+++ Project1/Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    public float ballInitVelocity = 600f;
+
+    private Rigidbody rb;
+    private bool ballInPlay;
+	// Use this for initialization
+	void Awake () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetButtonDown("Fire1") && ballInPlay == false)
+        {
+            transform.parent = null;
+            ballInPlay = true;
+            rb.isKinematic = false;
+            rb.AddForce(new Vector3(ballInitVelocity, ballInitVelocity, 0));
+        }
+	}
+}
---
+++ Project1/Assets/Scripts/GM.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GM : MonoBehaviour {
+    public int Lives = 3;
+    public int bricks = 24;
+    public float resetDelay = 1f;
+    public Text livesText;
+    public GameObject GameOver;
+    public GameObject Win;
+    public GameObject bricksPrefab;
+    public GameObject paddle;
+    public GameObject deathParticles;
+    public static GM instance = null;
+
+    private GameObject clonePaddle;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        Setup();
+	}
+
+    public void Setup()
+    {
+        clonePaddle = Instantiate(paddle, transform.position, Quaternion.identity) as GameObject;
+        Instantiate(bricksPrefab, new Vector3(-5.3f, -19.7f, 0f), Quaternion.identity);
+    }
+
+    void CheckGameOver()
+    {
+        if (bricks < 1)
+        {
+            Win.SetActive(true);
+            Time.timeScale = .25f;
+            Invoke("Reset", resetDelay);
+        }
+
+        if (Lives < 1)
+        {
+            GameOver.SetActive(true);
+            Time.timeScale = .25f;
+            Invoke("Reset", resetDelay);
+        }
+    }
+
+    void Reset()
+    {
+        Time.timeScale = 1f;
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void LoseLife()
+    {
+        Lives--;
+        livesText.text = "Lives: " + Lives;
+        Instantiate(deathParticles, clonePaddle.transform.position, Quaternion.identity);
+        Destroy(clonePaddle);
+        Invoke("SetupPaddle", resetDelay);
+        CheckGameOver();
+    }
+	
+    void SetupPaddle()
+    {
+        clonePaddle = Instantiate(paddle, transform.position, Quaternion.identity) as GameObject;
+    }
+
+    public void DestroyBrick()
+    {
+        bricks--;
+        CheckGameOver();
+    }
+}
